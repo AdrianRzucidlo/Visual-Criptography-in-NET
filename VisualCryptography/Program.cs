4770d744220256832b61fff1fa48724e31594bee
Adrian Rzucidło <adrianrzucidlo2001@gmail.com>
2023-04-14 22:44:47 +0200

    Project files

---
+++ VisualCryptography/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using VisualCryptography;
+
+Console.WriteLine("Kryptografia wizualna");
+while(true)
+{
+    Console.WriteLine("Chcesz zaszyfrować obrazek czy odszyfrować go?(Z/O)");
+    var choice = Console.ReadLine();
+    switch (choice.ToUpper())
+    {
+        case "Z":
+            Console.WriteLine("Podaj ścieżkę do pliku, który ma zostać podzielony na dwa obrazy:");
+            var path = Console.ReadLine();
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Zła ścieżka.");
+                return;
+            }
+            VisualCriptographyAlgorithm.Encrypt(path);
+            Console.WriteLine("Obrazek zaszyfrowany!");
+            break;
+
+        case "O":
+            Console.WriteLine("Podaj ścieżkę do pierwszego pliku: ");
+            var path1 = Console.ReadLine();
+            if (!File.Exists(path1))
+            {
+                Console.WriteLine("Zła ścieżka.");
+                return;
+            }
+            Console.WriteLine("Podaj ścieżkę do drugiego pliku: ");
+            var path2 = Console.ReadLine();
+            if (!File.Exists(path2))
+            {
+                Console.WriteLine("Zła ścieżka.");
+                return;
+            }
+            VisualCriptographyAlgorithm.Decrypt(path1,path2);
+            Console.WriteLine("Obrazek odszyfrowany");
+            break;
+        default: Console.WriteLine("Nieodpowiedni wybór."); return;
+    }
+}
---
+++ VisualCryptography/VisualCriptographyAlgorithm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualCryptography
+{
+    internal class VisualCriptographyAlgorithm
+    {
+        public static void Encrypt(string path)
+        {
+            Bitmap myImageBitMap = new Bitmap(path);
+            var bitMap = new int[100, 100];
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    bitMap[i, j] = myImageBitMap.GetPixel(i, j).R == 0 ? 1 : 0;
+                }
+            }
+            var rnd = new Random();
+            var firstImage = new Bitmap(100, 100);
+            var secondImage = new Bitmap(100, 100);
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    if (bitMap[i, j] == 0)
+                    {
+                        var r = rnd.Next(0, 100) % 2;
+                        if (r == 0)
+                        {
+                            firstImage.SetPixel(i, j, Color.White);
+                            secondImage.SetPixel(i, j, Color.Black);
+                        }
+                        else
+                        {
+                            firstImage.SetPixel(i, j, Color.Black);
+                            secondImage.SetPixel(i, j, Color.White);
+                        }
+                    }
+                    if (bitMap[i, j] == 1)
+                    {
+                        var r = rnd.Next(0, 100) % 2;
+                        if (r == 0)
+                        {
+                            firstImage.SetPixel(i, j, Color.White);
+                            secondImage.SetPixel(i, j, Color.White);
+                        }
+                        else
+                        {
+                            firstImage.SetPixel(i, j, Color.Black);
+                            secondImage.SetPixel(i, j, Color.Black);
+                        }
+                    }
+                }
+            }
+            firstImage.Save("firstEncrypted.png", ImageFormat.Png);
+            secondImage.Save("secondEncrypted.png", ImageFormat.Png);
+        }
+
+        public static void Decrypt(string firstPath,string secondPath)
+        {
+            var firstBitMap = (Bitmap)Bitmap.FromFile(firstPath);
+            var secondBitMap = (Bitmap)Bitmap.FromFile(secondPath);
+            Bitmap resultBitMap = new Bitmap(100,100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    if (firstBitMap.GetPixel(i, j) == secondBitMap.GetPixel(i, j))
+                    {
+                        resultBitMap.SetPixel(j, i, firstBitMap.GetPixel(i, j));
+                    }
+                    else
+                    {
+                        resultBitMap.SetPixel(j, i, Color.Gray);
+                    }
+                }
+            }
+            resultBitMap.Save("decryptedImage.png", ImageFormat.Png);
+        }
+    }
+}
